c4514adf3846b47e3be49c507442692f55533f72
Janko Klikovac <jankoklikovac@gmail.com>
2025-07-11 13:48:28 +0200

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'library/static'),
+    os.path.join(BASE_DIR,'LMS', 'library', 'static'),
 ]
 
 # Default primary key field type
